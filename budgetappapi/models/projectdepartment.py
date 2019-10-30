0c9efc10d5ebb104e88df73ba0930d440d83d473
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-10-30 09:21:21 -0500

    update model

--- budgetappapi/models/projectdepartment.py
+++ budgetappapi/models/projectdepartment.py
@@ -7,7 +7,7 @@ class ProjectDepartment(models.Model):
 
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     department_hour = models.ForeignKey(DepartmentHour, on_delete=models.CASCADE, null=True)
-    project_budget = models.ForeignKey(ProjectBudget, on_delete=models.CASCADE)
+    project_budget = models.ForeignKey(ProjectBudget, on_delete=models.DO_NOTHING)
 
     class Meta:
             verbose_name = ("ProjectDepartment")
