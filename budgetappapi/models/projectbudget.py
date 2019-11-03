74fdc4272c74bbc59c9d44935ca2bcfd89495524
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-11-03 16:10:20 -0600

    minor changes, still need projectbudget to pull over correct data

--- budgetappapi/models/projectbudget.py
+++ budgetappapi/models/projectbudget.py
@@ -18,25 +18,32 @@ class ProjectBudget(models.Model):
     @property
     def weekly_cost(self):
         projectDepts = ProjectDepartment.objects.filter(project_budget_id=self)
-        rate = projectDepts.department.aggregate(sum=Sum('rate'))
-        return rate
+        print(projectDepts)
+        # return projectDepts.department.aggregate(total_rate = Sum('rate'))
+        # rate = projectDepts.department.aggregate(total_rate = Sum('rate'))
+
+        # return projectDepts.department.rate
         # project_rate = ProjectDepartment.objects.filter(project_budget_id=self).annotate(rate=Sum('') )
         # project_rate = Sum(Case(When(stock__ttype='I', then=F('stock__quantity')), output_field=DecimalField(), default=0)),
 
         weekly_cost = 0
-        # length = 0
+        length = 0
+        rate = 0
         for projectDept in projectDepts:
-            # departments = Department.objects.filter(id=projectDept.id)
+            departments = Department.objects.filter(id=projectDept.id)
+            print(departments)
+            rate += departments.rate
+            return rate
             # for dept in departments:
-                # print(dept.rate)
-                # rate += dept.rate
+            #     rate += dept.rate
+
             # length += projectDept.department.length
             # weekly_time = projectDept.quantity * 40
 
             # sum(projectDept.department.rate)
             # weekly_cost = weekly_time * rate
             # return weekly_cost
-            print(rate)
+            # print(rate)
 
     # @property
     # def monthly_cost(self):
--- budgetappapi/models/projectdepartment.py
+++ budgetappapi/models/projectdepartment.py
@@ -1,8 +1,7 @@
 from django.db import models
-
 class ProjectDepartment(models.Model):
 
-    department = models.ForeignKey("Department", on_delete=models.CASCADE)
+    department = models.ForeignKey("Department", on_delete=models.DO_NOTHING)
     department_hour = models.ForeignKey("DepartmentHour", on_delete=models.CASCADE, null=True)
     project_budget = models.ForeignKey("ProjectBudget", on_delete=models.DO_NOTHING)
     quantity = models.IntegerField(null=True)
@@ -10,4 +9,25 @@ class ProjectDepartment(models.Model):
     class Meta:
             verbose_name = ("ProjectDepartment")
             verbose_name_plural = ("ProjectDepartments")
+
+    @property
+    def weekly_cost(self):
+        rate = self.department.rate
+        quantity = self.department.quantity
+
+        weekly_time = quantity * 40
+        weekly_cost = weekly_time * rate
+        return weekly_cost
+
+    @property
+    def monthly_cost(self):
+        yearly_cost = self.weekly_cost * 52
+        monthly_cost = yearly_cost/12
+        return monthly_cost
+
+    @property
+    def total_cost(self):
+        length = self.project_budget.length
+        total_cost = self.monthly_cost * length
+        return total_cost
 
