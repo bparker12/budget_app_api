497e4a070284b5021a9d6934da8dfb0de6edcadd
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-11-03 17:42:13 -0600

    got it to pull the total rate

--- budgetappapi/models/projectbudget.py
+++ budgetappapi/models/projectbudget.py
@@ -15,35 +15,29 @@ class ProjectBudget(models.Model):
             verbose_name = ("ProjectBudget")
             verbose_name_plural = ("ProjectBudgets")
 
+    # @property
+    # def department(self):
+    #     projectDept = ProjectDepartment.objects.filter(project_budget_id=self)
+
+
+
+
+
+
+
+
     @property
     def weekly_cost(self):
         projectDepts = ProjectDepartment.objects.filter(project_budget_id=self)
         print(projectDepts)
-        # return projectDepts.department.aggregate(total_rate = Sum('rate'))
-        # rate = projectDepts.department.aggregate(total_rate = Sum('rate'))
 
-        # return projectDepts.department.rate
-        # project_rate = ProjectDepartment.objects.filter(project_budget_id=self).annotate(rate=Sum('') )
-        # project_rate = Sum(Case(When(stock__ttype='I', then=F('stock__quantity')), output_field=DecimalField(), default=0)),
+        rate = []
+        for projectDep in projectDepts:
+            print(projectDep.department.rate)
+            rate.append(projectDep.department.rate)
+            print(rate)
+        return sum(rate)
 
-        weekly_cost = 0
-        length = 0
-        rate = 0
-        for projectDept in projectDepts:
-            departments = Department.objects.filter(id=projectDept.id)
-            print(departments)
-            rate += departments.rate
-            return rate
-            # for dept in departments:
-            #     rate += dept.rate
-
-            # length += projectDept.department.length
-            # weekly_time = projectDept.quantity * 40
-
-            # sum(projectDept.department.rate)
-            # weekly_cost = weekly_time * rate
-            # return weekly_cost
-            # print(rate)
 
     # @property
     # def monthly_cost(self):
